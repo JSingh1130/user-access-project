5c293f36808e9993df50b36425664ce505d22cf3
jsingh830 <jsingh8_be21@thapar.edu>
2025-05-24 03:16:05 +0530

    Fix: updated DB key to match .env for deployment

--- user-access-system/src/ormconfig.ts
+++ user-access-system/src/ormconfig.ts
@@ -12,7 +12,7 @@ export const AppDataSource = new DataSource({
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
   synchronize: true,
   logging: false,
   entities: [User, Software, Request],
